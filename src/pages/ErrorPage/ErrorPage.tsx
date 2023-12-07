5e5ab4bdd737608f7886f762579a62b28b8a345e
Alla Prishchepa <tyurina.alla@gmail.com>
2023-12-08 00:26:34 +0400

    refactor: refactor error page and root styles

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,47 +1,28 @@
 import {
-  Link,
   isRouteErrorResponse,
   useNavigate,
   useRouteError,
 } from 'react-router-dom';
 import { HttpStatusCode } from '../../utils/enums';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 export default function ErrorPage() {
   const error = useRouteError();
   const navigate = useNavigate();
-
   const pageReload = () => navigate(0);
 
-  const notFound = (): JSX.Element => {
-    return (
-      <div className="error-page">
-        <div className="error-message">
-          <div>Sorry... The page is not found.</div>
-          <div>Please, go to Home page.</div>
-        </div>
-        <Link className="button home-link" to="/">
-          Go Home
-        </Link>
-      </div>
-    );
-  };
+  if (isRouteErrorResponse(error) && error.status === HttpStatusCode.NOT_FOUND)
+    return <NotFoundPage />;
 
-  const someError = (): JSX.Element => {
-    return (
-      <div className="error-page">
-        <div className="error-message">
-          <div>Sorry... The error occurred.</div>
-          <div>Please, try to reload page</div>
-        </div>
-        <button className="reload-button" onClick={pageReload}>
-          Reload page
-        </button>
+  return (
+    <div className="error-page">
+      <div className="error-message">
+        <div>Sorry... The error occurred.</div>
+        <div>Please, try to reload page</div>
       </div>
-    );
-  };
-
-  return isRouteErrorResponse(error) &&
-    error.status === HttpStatusCode.NOT_FOUND
-    ? notFound()
-    : someError();
+      <button className="reload-button" onClick={pageReload}>
+        Reload page
+      </button>
+    </div>
+  );
 }
