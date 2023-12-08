3739c053710e3c8b454b948d175fc4424e51f9ba
Yauheniya Mastavaya <jk@db.by>
2023-12-08 21:15:26 +0400

    chore: add tests for welcome page

---
+++ __tests__/welcome.test.tsx
@@ -0,0 +1,55 @@
+import { describe, it, expect } from 'vitest';
+import { routesConfig } from '../src/router/router';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import Hero from '../src/components/Hero/Hero';
+import Features from '../src/components/Features/Features';
+import Team from '../src/components/Team/Team';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+describe('Welcome Page', () => {
+  it('Hero displayed on welcome page', async () => {
+    render(<Hero />);
+
+    const graphiQL = await screen.findByText('GraphiQL');
+    expect(graphiQL).toBeInTheDocument();
+  });
+
+  it('Features displayed on welcome page', async () => {
+    render(<Features />);
+
+    const features = await screen.findByText(/Features/);
+    expect(features).toBeInTheDocument();
+  });
+
+  it('Our team displayed on welcome page', async () => {
+    render(<Team />);
+
+    const team = await screen.findByText(/Our team/);
+    expect(team).toBeInTheDocument();
+  });
+
+  it('Should navigate to page Sign In on button click', async () => {
+    const router = createMemoryRouter(routesConfig);
+
+    render(<RouterProvider router={router} />);
+    const user = userEvent.setup();
+
+    expect(screen.getByText(/Sign In/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText(/Sign In/i));
+    expect(screen.getByText(/Sign In/i)).toBeInTheDocument();
+  });
+
+  it('Should navigate to page Sign Up on button click', async () => {
+    const router = createMemoryRouter(routesConfig);
+
+    render(<RouterProvider router={router} />);
+    const user = userEvent.setup();
+
+    expect(screen.getByText(/Sign Up/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText(/Sign Up/i));
+    expect(screen.getByText(/Sign Up/i)).toBeInTheDocument();
+  });
+});
