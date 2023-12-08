b284253125d7808e45abfa034c5dd1ab83dc79bd
Yauheniya Mastavaya <jk@db.by>
2023-12-08 20:46:15 +0400

    feat: add sign in and sign up buttons

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -3,21 +3,30 @@ import { AppRoutes } from '../../router/router';
 import Features from '../../components/Features/Features';
 import Hero from '../../components/Hero/Hero';
 import Team from '../../components/Team/Team';
+import './Welcom.scss';
 
 export default function Welcome() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to={AppRoutes.signIn}>Sign In</Link>
-        </li>
-        <li>
-          <Link to={AppRoutes.signUp}>Sign Up</Link>
-        </li>
-        <li>
+      <div className="header">
+        <div className="container">
+          <ul>
+            <li>
+              <Link to={AppRoutes.signIn} className="btn">
+                Sign In
+              </Link>
+            </li>
+            <li>
+              <Link to={AppRoutes.signUp} className="btn">
+                Sign Up
+              </Link>
+            </li>
+            {/* <li>
           <Link to={AppRoutes.main}>Main</Link>
-        </li>
-      </ul>
+        </li> */}
+          </ul>
+        </div>
+      </div>
       <Hero />
       <Features />
       <Team />
