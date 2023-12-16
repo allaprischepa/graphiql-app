4429e35b06cce02726041b95be4279241bcb5d41
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-12-16 21:24:19 +0300

    feat: add header to the main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,6 +5,7 @@ import { useRef } from 'react';
 import { EditorView } from 'codemirror';
 import './Main.scss';
 import EditorTools from '../../components/EditorTools/EditorTools';
+import Header from '../../components/Header/Header';
 
 export const TEST_ID = 'main-page';
 export const REQUEST_SECTION_TEST_ID = 'request-section';
@@ -17,39 +18,42 @@ export default function Main() {
   const headersViewRef = useRef<EditorView | null>(null);
 
   return (
-    <main className="graphiql-container" data-testid={TEST_ID}>
-      <section className="graphiql-main">
-        <section
-          className="request-section"
-          data-testid={REQUEST_SECTION_TEST_ID}
-        >
-          <div className="editor-section">
-            <QueryEditor
-              viewRef={requestViewRef}
-              mode="request"
-              text={defaultQueryString}
-            />
-            <ControlPanel
-              requestViewRef={requestViewRef}
-              responseViewRef={responseViewRef}
-              variablesViewRef={variablesViewRef}
-              headersViewRef={headersViewRef}
-            />
-          </div>
-          <div className="tools-section">
-            <EditorTools
-              variablesViewRef={variablesViewRef}
-              headersViewRef={headersViewRef}
-            />
-          </div>
+    <>
+      <Header />
+      <main className="graphiql-container" data-testid={TEST_ID}>
+        <section className="graphiql-main">
+          <section
+            className="request-section"
+            data-testid={REQUEST_SECTION_TEST_ID}
+          >
+            <div className="editor-section">
+              <QueryEditor
+                viewRef={requestViewRef}
+                mode="request"
+                text={defaultQueryString}
+              />
+              <ControlPanel
+                requestViewRef={requestViewRef}
+                responseViewRef={responseViewRef}
+                variablesViewRef={variablesViewRef}
+                headersViewRef={headersViewRef}
+              />
+            </div>
+            <div className="tools-section">
+              <EditorTools
+                variablesViewRef={variablesViewRef}
+                headersViewRef={headersViewRef}
+              />
+            </div>
+          </section>
+          <section
+            className="response-section"
+            data-testid={RESPONSE_SECTION_TEST_ID}
+          >
+            <QueryEditor viewRef={responseViewRef} mode="response" />
+          </section>
         </section>
-        <section
-          className="response-section"
-          data-testid={RESPONSE_SECTION_TEST_ID}
-        >
-          <QueryEditor viewRef={responseViewRef} mode="response" />
-        </section>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
