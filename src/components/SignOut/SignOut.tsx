153a658e2814c4b49b60ad0a6372a12b9c8dc36d
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-12-17 14:37:12 +0300

    feat: add redirect after token expiring

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -1,20 +1,27 @@
 import { userAuth } from '../../services/firebaseAuth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setIsUserLoggedIn } from '../../services/authSlice';
 import { AppRoutes } from '../../../src/utils/enums';
 
 import './SignOut.scss';
-import { setIsUserLoggedIn } from '../../services/authSlice';
 
 const SignOut = () => {
+  const auth = getAuth();
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const userLogOut = async (): Promise<void> => {
     try {
       await userAuth.logOut();
-      navigate(AppRoutes.welcome);
-      dispatch(setIsUserLoggedIn(false));
+      onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          dispatch(setIsUserLoggedIn(false));
+          navigate(AppRoutes.welcome);
+        }
+      });
     } catch (err) {
       alert(err);
     }
